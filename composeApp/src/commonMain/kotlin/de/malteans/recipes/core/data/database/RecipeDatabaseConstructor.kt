46caa276d072aeaab9eb570ced1cff5f280b1d4f
Malte <development@malteans.de>
2025-11-24 12:46:05 +0100

    fix: composeApp CI-Pipeline

--- composeApp/src/commonMain/kotlin/de/malteans/recipes/core/data/database/RecipeDatabaseConstructor.kt
+++ composeApp/src/commonMain/kotlin/de/malteans/recipes/core/data/database/RecipeDatabaseConstructor.kt
@@ -3,4 +3,6 @@ package de.malteans.recipes.core.data.database
 import androidx.room.RoomDatabaseConstructor
 
 @Suppress("NO_ACTUAL_FOR_EXPECT")
-expect object RecipeDatabaseConstructor: RoomDatabaseConstructor<RecipeDatabase>
+expect object RecipeDatabaseConstructor: RoomDatabaseConstructor<RecipeDatabase> {
+    override fun initialize(): RecipeDatabase
+}
