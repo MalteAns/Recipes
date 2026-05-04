2e33d27b74dd4d55cdb1c134935dd1be7e4d3234
Malte <development@malteans.de>
2026-05-04 11:45:11 +0200

    small fixes + added JKS file

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -39,7 +39,7 @@ val generateDesktopBuildConfig = tasks.register("generateDesktopBuildConfig") {
             package de.malteans.recipes.core.data.network
 
             object DesktopBuildConfig {
-                const val API_TOKEN = "$apiToken"
+                const val API_TOKEN = $apiToken
             }
             """.trimIndent()
         )
